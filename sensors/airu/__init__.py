bdca6b6590483c4d386ca2c33588fb923e689dd7
Philip Lundrigan <philiplundrigan@gmail.com>
2017-05-25 14:44:11 -0600

    Finish airu sensor

--- sensors/airu/__init__.py
+++
@@ -1,6 +0,0 @@
-REQUIREMENTS = ['https://github.com/adafruit/Adafruit_Python_DHT/archive/master.zip#Adafruit_Python_DHT==1.3.2']
-
-def setup_sensor(config):
-    from .airstation import AirStation
-
-    return AirStation()
